6bd54b6084875afab1a0f84bd87540b7dc18f1c8
test <test@test.test>
2021-04-15 19:09:41 +0800

    docker-compose

--- docker-compose-init.sh
+++ docker-compose-init.sh
@@ -27,12 +27,14 @@ docker-compose start graph-node
 CONTAINER_ID=$(docker container ls | grep graph-node | cut -d' ' -f1)
 
 # Inspect the container to identify the host IP address
-HOST_IP=$(docker inspect "$CONTAINER_ID" | jq -r .[0].NetworkSettings.Networks[].Gateway)
+echo $(docker inspect "$CONTAINER_ID")
+echo "Host IP: $CONTAINER_ID"
+#HOST_IP=$(docker inspect "$CONTAINER_ID" | jq -r .[0].NetworkSettings.Networks[].Gateway)
 
 echo "Host IP: $HOST_IP"
 
 # Inject the host IP into docker-compose.yml
-sed -i -e "s/host.docker.internal/$HOST_IP/g" docker-compose.yml
+#sed -i -e "s/host.docker.internal/$HOST_IP/g" docker-compose.yml
 
 function stop_graph_node {
     # Ensure graph-node is stopped
@@ -40,3 +42,5 @@ function stop_graph_node {
 }
 
 trap stop_graph_node EXIT
+
+rm -rf .data
