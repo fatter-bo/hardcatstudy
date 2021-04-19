a87f56c973c61419d979769b6e54b03d1e56a634
test <test@test.test>
2021-04-19 11:57:13 +0800

    hardhat.config.ts.example

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npx hardhat run --network ganache  ./scripts/deploy.ts
-#npx hardhat run --network bsc-testnet  ./scripts/deploy.ts
+#npx hardhat run --network bsctestnet  ./scripts/deploy.ts
 #npx hardhat run --network bsc  ./scripts/deploy.ts
