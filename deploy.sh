0e98214b64f0b44379e5a68d34e60416cbe654db
test <test@test.test>
2021-04-19 10:39:27 +0800

    bsc testnet

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 npx hardhat run --network ganache  ./scripts/deploy.ts
+#npx hardhat run --network bsc-testnet  ./scripts/deploy.ts
 #npx hardhat run --network bsc  ./scripts/deploy.ts
